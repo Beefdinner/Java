d5596294ee5ed51fc3edfc376bc5f3672bcf6176
Beefdinner <jonathan.turner.m@hotmail.com>
2018-01-25 10:44:59 -0600

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,28 @@
+// Variables
+// Jon Turner
+
+public class Variables
+{
+
+	public static void (String[] args)
+	{
+	int testGrade = 100;
+	long cityPopulation = 123456789L;
+	byte ageInYears = 10;
+	
+	float salesTax = 0.025f;
+	double interestRate = 0.025;
+	double avogdroNumber = +6.022E23;
+	
+	char firstLetter = 'A';
+	boolean isEmpty = false;
+	
+	system.out.println("testGrade is " + testGrade);
+	system.out.println("cityPopulation is " + cityPopulation);
+	system.out.println("Age in years " + ageInYears);
+	system.out.println("Sales tax is " + salesTax);
+	system.out.println("Interest rate is " + intrestRate);
+	system.out.println("avgdronumber is " + avogdroNumber);
+	system.out.println("First letter is" + firstLetter);
+	system.out.println("isEmpty is" + isEmpty);
+	}
