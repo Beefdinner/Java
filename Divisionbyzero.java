9fcc6390dcfadce5bcc00f90cef3b469f9177117
Beefdinner <jonathan.turner.m@hotmail.com>
2018-01-30 10:31:52 -0600

    Add files via upload

---
+++ Divisionbyzero.java
@@ -0,0 +1,17 @@
+
+public class Divisionbyzero {
+
+	public static void main(String[] args) 
+	{
+		double result1 = 4.3 / 0.0;
+		System.out.println("Result1 is " + result1);
+		
+		double result2 = 0.0 /0.0;
+		System.out.println("Result2 is " + result2);
+
+		int result3 = 4 / 0;
+		System.out.println("Result3 is " + result3);
+
+	}
+
+}
