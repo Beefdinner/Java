2b8e4dc99697f9bed4237cc90d4c88eb4268c206
Beefdinner <jonathan.turner.m@hotmail.com>
2018-01-30 14:01:07 -0600

    Add files via upload

---
+++ InputsOuputs.java
@@ -0,0 +1,16 @@
+
+public class InputsOuputs {
+
+	public static void main(String[] args) 
+	{
+	final double CM_PER_INCH = 2.54;
+	
+	double inches = 10;
+	
+	double centimeters = inches * CM_PER_INCH;
+	
+	System.out.println(inches + " inches equals " + centimeters + " centimeters");
+		
+	}
+
+}
