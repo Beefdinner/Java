ae5e39a61b244b6a8343864ab7f4a380691ea143
Beefdinner <jonathan.turner.m@hotmail.com>
2018-01-30 11:02:07 -0600

    Add files via upload

---
+++ ShortcutOperators.java
@@ -0,0 +1,19 @@
+
+public class ShortcutOperators {
+
+	public static void main(String[] args) 
+	{
+		int a = 6;
+		int b = 2;
+		
+		System.out.println("At the begining , a is " + a);
+		System.out.println("Increment a with a prefix notation:  " + ++a);
+		System.out.println("In the end A is " + a);
+		
+		System.out.println("\nAt the begining , b is " + b);
+		System.out.println("Increment with a postfix notation now b is " + b++);
+		System.out.println("In the end b is " + b);
+		
+	}
+
+}
